e6b24bcff3adfa4f49646cf72e79f605be5161e7
olkens <olkusz.michal@gmail.com>
2025-08-13 21:29:29 +0200

    Added a way to send payment to backend and save it

--- components/ui/PaymentsTab.tsx
+++ components/ui/PaymentsTab.tsx
@@ -14,9 +14,19 @@ export default function PaymentData(props: { paymentsData: Payment[] }) {
     paymentsLeft: 0,
     numberOfPayments: 0,
   };
-  const handleSubmit = (payment: Payment) => {
-    console.log("Dane wysłane:", payment);
-    setPayments([...payments, payment]);
+  const handleSubmit = async (payment: Payment) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payment),
+    };
+    const response = await fetch(
+      "http://localhost:8080/api/v1/payments",
+      requestOptions,
+    );
+    const savedPayment = await response.json();
+
+    setPayments([...payments, savedPayment]);
   };
 
   return (
