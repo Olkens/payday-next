2f1a69c2fc032a4cab08bbfa65d8de0303c7bee5
olkens <olkusz.michal@gmail.com>
2025-08-18 22:44:06 +0200

    changed path for month data to include year variable

--- app/month/[yearId]/[monthId]/page.tsx
+++ app/month/[yearId]/[monthId]/page.tsx
@@ -5,11 +5,15 @@ import { Payment } from "@/lib/types";
 export default async function MonthPage({
   params,
 }: {
-  params: Promise<{ monthId: number; name: string }>;
+  params: Promise<{ monthId: number; name: string; yearId: number }>;
 }) {
-  const { monthId } = await params;
-  const data = await fetch("http://localhost:8080/api/v1/payments");
+  const { monthId, yearId } = await params;
+
+  const data = await fetch(
+    `http://localhost:8080/api/v1/payments/${yearId}/${monthId}`,
+  );
   const payments: Payment[] = await data.json();
+  console.log(payments);
 
   return (
     <div className="w-3/5 flex flex-col items-center justify-center">
